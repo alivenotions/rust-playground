98b9884fd2898ad990f0145bcdb675f3653ebc0e
Bhavdeep Dhanjal <dhanjalbhavdeep@gmail.com>
2019-03-22 08:59:12 +0530

    fix comments

--- common-concepts/src/main.rs
+++ common-concepts/src/main.rs
@@ -37,16 +37,18 @@ fn main() {
     // the value depending on the value of x before
     // this. According to me this can be a source
     // of confusion but it is useful for using the
-    // same variable and mutating the type.
+    // same variable name and mutating the type.
     let x = x + 5;
     println!("The value of x is {}", x);
 
     // if this variable is not annotated with a type
-    // then it will through an error because it cannot
+    // then it will throw an error because it cannot
     // infer it.
     let y: u32 = "34".parse().expect("Not a number");
 
     // Scalar types
+    // Types like signed and unsigned integers, floats,
+    // chars, bool etc.
     let z: i32 = -33;
     println!(
         "The unsigned value is: {} while the signed value is: {}",
@@ -56,7 +58,7 @@ fn main() {
     // Tuple is a type of compound type
     // A tuple can have elements of different types
     let a_tuple_type: (u32, f64, char) = (500, 65.2, 'a');
-    // tuple supports destrcuturing
+    // tuple supports destructuring
     let (first, second, third) = a_tuple_type;
     println!(
         "The destructured vars are: {}, {}, {}",
@@ -71,9 +73,8 @@ fn main() {
 
     // An array type is also a compound type but it
     // only allows for homogenous types in the array.
-    // Arrays are allocated on the stack. Arrays are
-    // always of fixed type. If you need a collection
-    // that grows or shrinks, you need a vector.
+    // Arrays are allocated on stack. If you need a
+    // collection that grows/shrinks, you need a vector.
     let first_array: [u8; 4] = [1, 2, 3, 4];
     // Need to include :? inside the formatter parens to print an array.
     // TODO: Need to find why we need to do that.
